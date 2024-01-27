dd036a136f56a1ea32172f5dece05e44d931e555
ibrahim29asad <ibrahim.asad2019@gmail.com>
2024-01-26 17:42:20 -0700

    Typing is not working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import background from "../src/images/back.jpg";
+import ProfilePic from "../src/images/profile.jpg"
+import { LinearGradient } from 'react-text-gradients';
+import { TypeAnimation } from 'react-type-animation';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+
+    
+    <div className="App" style={{ backgroundImage: `url(${background})`
+    , backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    
+    }}>
+<div className='Header-Style'>
+    <Header/>  
+    </div> 
+
+  <div className='Home-Text'>
+    Hey There ! &nbsp;
+    <div className='wave'>
+    😁 
+    </div>
+    <div className='wave'>
+      👋   
+    </div>
+    <br/>
+      I'm &nbsp;
+    <LinearGradient gradient={['to right', '#0acdf0 ,#ffffff']} >
+      Ibrahim
+    </LinearGradient>
+
+
+   
+
+       
+  
+  </div>
+
+
+
+    </div>
+   
+  );
+}
+
+export default App;
+
+
+          {/* 
+          <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,8 +68,18 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
-}
-
-export default App;
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> 
+     */}
